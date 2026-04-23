0c96bfbe18231f8e182f3c2cde9840524b7a415d
jimmy613 <98528433+jimmy613@users.noreply.github.com>
2026-04-23 19:16:23 +0800

    Add files via upload

---
+++ CakeShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CakeShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CakeShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cake> Cakes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
