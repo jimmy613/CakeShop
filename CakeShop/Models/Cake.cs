9293edd57cbf9c5bc39f51fb561fd89a91c9eaf9
jimmy613 <98528433+jimmy613@users.noreply.github.com>
2026-04-23 19:16:02 +0800

    Add files via upload

---
+++ CakeShop/Models/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace CakeShop.Models
+{
+    public class Cake
+    {
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+            public string ImageUrl { get; set; }
+
+        }
+    
+}
+
