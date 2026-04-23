f82e9ae6e71d36d3f7d68eb709300d59007e3ab6
jimmy613 <98528433+jimmy613@users.noreply.github.com>
2026-04-23 21:21:44 +0800

    Add files via upload

--- CakeShop/Models/Cake.cs
+++ CakeShop/Models/Cake.cs
@@ -7,12 +7,12 @@ namespace CakeShop.Models
     {
 
             public int Id { get; set; }
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public decimal Price { get; set; }
-            public string ImageUrl { get; set; }
+            public string Name { get; set; } = string.Empty;
+             public decimal Price { get; set; }
+            public string Description { get; set; } = string.Empty;
+            public string ImageUrl { get; set; } = string.Empty;
 
-        }
+    }
     
 }
 
